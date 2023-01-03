568a1d39e27a2fed332560ec6b1611ddcc365a1a
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-01-03 20:18:41 +0100

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: 'json' } do
+    get 'message', to: 'greetings#random'
+  end
+
   get 'root/index'
   get 'greetings/random'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
