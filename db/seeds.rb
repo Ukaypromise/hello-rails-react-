a40bacd43020a76570d759e31224cde10ee84b61
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2023-01-03 19:04:34 +0100

    Update route paths and configure assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: "Welcome to Rails with React!")
+Greeting.create(message: "Hello My Frien!")
+Greeting.create(message: "Hello World of rails webpack!")
+Greeting.create(message: "Hello To Rails with React")
+Greeting.create(message: "Hello once again")
